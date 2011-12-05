5af9e166ff3a21c4c249668f5e5c3ce23dbfbc77
Andrew Ellis <awellis@me.com>
2011-12-05 12:35:27 -0700

    minor tweaks

--- wp-social-debug.php
+++ wp-social-debug.php
@@ -26,9 +26,21 @@ function wp_social_debug_init() {
 			if (is_array($post) and isset($post[0])) {
 				$post = $post[0];
 
+				echo '<pre>';
 				echo '-- Post:<br /><br />';
 				echo 'INSERT INTO '.$wpdb->posts.'(ID, post_author, post_date, post_date_gmt, post_content, post_title, post_excerpt, post_status, comment_status, ping_status, post_password, post_name, to_ping, pinged, post_modified, post_modified_gmt, post_content_filtered, post_parent, guid, menu_order, post_type, post_mime_type, comment_count)'.
-					" VALUES('$post->ID', '$post->post_author', '$post->post_date', '$post->post_date_gmt', '$post->post_content', '$post->post_title', '$post->post_excerpt', '$post->post_status', '$post->comment_status', '$post->ping_status', '$post->post_password', '$post->post_name', '$post->to_ping', '$post->pinged', '$post->post_modified', '$post->post_modified_gmt', '$post->post_content_filtered', '$post->post_parent', '$post->guid', '$post->menu_order', '$post->post_type', '$post->post_mime_type', '$post->comment_count');<br />";
+					" VALUES('$post->ID', '".htmlentities($post->post_author)."', '$post->post_date', '$post->post_date_gmt', '".htmlentities($post->post_content)."', '".htmlentities($post->post_title)."', '".htmlentities($post->post_excerpt)."', '".htmlentities($post->post_status)."', '".htmlentities($post->comment_status)."', '".htmlentities($post->post_status)."', '$post->post_password', '".htmlentities($post->post_name)."', '$post->to_ping', '$post->pinged', '$post->post_modified', '$post->post_modified_gmt', '".htmlentities($post->post_content_filtered)."', '$post->post_parent', '".htmlentities($post->post_guid)."', '$post->menu_order', '".htmlentities($post->post_type)."', '".htmlentities($post->post_mime_type)."', '$post->comment_count');<br />";
+
+				$results = $wpdb->get_results("
+					SELECT *
+					  FROM $wpdb->postmeta
+					 WHERE post_id = $post_id
+				");
+
+				echo '<br /><br />-- Post Meta:<br /><br />';
+				foreach ($results as $result) {
+					echo 'INSERT INTO '.$wpdb->postmeta.'(post_id, meta_key, meta_value)'." VALUES('$result->post_id', '".htmlentities($result->meta_key)."', '".htmlentities($result->meta_value)."');<br />";
+				}
 
 				$comment_ids = array();
 				$comments = $wpdb->get_results("
@@ -36,12 +48,12 @@ function wp_social_debug_init() {
 					  FROM $wpdb->comments
 					 WHERE comment_post_ID = $post_id
 				");
-				echo '-- Comments:<br /><br />';
+				echo '<br /><br />-- Comments:<br /><br />';
 				foreach ($comments as $comment) {
 					$comment_ids[] = $comment->comment_ID;
 
 					echo 'INSERT INTO '.$wpdb->comments.'(comment_ID, comment_post_ID, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_date_gmt, comment_content, comment_karma, comment_approved, comment_agent, comment_type, comment_parent, user_id)'.
-						" VALUES('$comment->comment_ID', '$comment->comment_post_ID', '$comment->comment_author', '$comment->comment_author_email', '$comment->comment_auth_url', '$comment->comment_author_IP', '$comment->comment_date', '$comment->comment_date_gmt', '$comment->comment_content', '$comment->comment_karma', '$comment->comment_approved', '$comment->comment_agent', '$comment->comment_type', '$comment->comment_parent', '$comment->user_id');<br />";
+						" VALUES('$comment->comment_ID', '$comment->comment_post_ID', '".htmlentities($comment->comment_author)."', '".htmlentities($comment->comment_author_email)."', '', '$comment->comment_author_url', '".htmlentities($comment->comment_author_IP)."', '$comment->comment_date', '$comment->comment_date_gmt', '".htmlentities($comment->comment_content)."', '$comment->comment_karma', '$comment->comment_approved', '".htmlentities($comment->comment_agent)."', '".htmlentities($comment->comment_type).", '$comment->comment_parent', '$comment->user_id');<br />";
 				}
 
 				$comment_ids = implode(',', $comment_ids);
@@ -50,9 +62,11 @@ function wp_social_debug_init() {
 					  FROM $wpdb->commentmeta
 					 WHERE comment_id IN ($comment_ids)
 				");
+				echo '<br /><br />-- Comment Meta:<br /><br />';
 				foreach ($results as $result) {
-					echo 'INSERT INTO '.$wpdb->commentmeta.'(comment_id, meta_key, meta_value)'." VALUES('$result->comment_id', '$result->meta_key', '$result->meta_value');<br />";
+					echo 'INSERT INTO '.$wpdb->commentmeta.'(comment_id, meta_key, meta_value)'." VALUES('$result->comment_id', '".htmlentities($result->meta_key)."', '".htmlentities($result->meta_value)."');<br />";
 				}
+				echo '</pre>';
 			}
 		}
 		else {
